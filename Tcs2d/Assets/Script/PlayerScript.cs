4c5cc58ecafb17b9f7ccb7274a7a1e1f272a8809
aman654 <58172216+aman654@users.noreply.github.com>
2020-02-03 11:20:46 +0530

    commit2

--- Tcs2d/Assets/Script/PlayerScript.cs
+++ Tcs2d/Assets/Script/PlayerScript.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rb;
    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
-        
+        //move Player
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
+        //
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = mousePos-rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x)*Mathf.Rad2Deg-90f;
+        rb.rotation = angle;
+
     }
+
+
 }
